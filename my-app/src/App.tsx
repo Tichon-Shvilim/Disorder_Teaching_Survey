abb7645e38fde10b295d05d16bc928857931a820
HennyGreenberg <h9913578@gmail.com>
2025-06-24 16:58:33 +0300

    first comit

---
+++ my-app/src/App.tsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import SignUp from './sign-up/SignUp';
+
+function App() {
+  //const [count, setCount] = useState(0)
+
+  return (
+    <><SignUp></SignUp>
+
+      
+     
+    </>
+  )
+}
+
+export default App
