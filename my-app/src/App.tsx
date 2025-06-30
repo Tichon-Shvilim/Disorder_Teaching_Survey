5e857c898f567c474b4bd1db9608ff81874178d0
HennyGreenberg <h9913578@gmail.com>
2025-06-30 14:32:42 +0300

    working on student list component

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -2,6 +2,7 @@ import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import SignIn from './components/signIn/SignIn';
 import SignUp from './components/signUp/SignUp';
+import StudentList from './components/studentManagement/StudentList';
 
 function App() {
   return (
@@ -10,6 +11,16 @@ function App() {
         <Route path="/" element={<SignUp />} />
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp/>} />
+        <Route
+          path="/studentlist"
+          element={
+            <StudentList
+              users={[]} // Replace with your users data
+              onEdit={() => {}} // Replace with your edit handler
+              onDelete={() => {}} // Replace with your delete handler
+            />
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
